02552ca5f0405dc82cb57ccc3fb283b814d1000d
Maria Faisal <bcsf17m517@pucit.edu.pk>
2020-06-24 12:48:21 +0500

    mongo db connected:showing the collection as well

--- app.js
+++ app.js
@@ -8,13 +8,11 @@ var NaMe= "";
 const app = express();
 
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({ extended: true }))
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/mariadb', {useNewUrlParser: true});
 
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
-
 db.once("open", function() {
 console.log("Connection Successful!");
 });
@@ -23,13 +21,10 @@ const userSchema = {
     name : String
 };
 
-const User = mongoose.model("user", userSchema );
+const user = mongoose.model("user", userSchema );
 
-var u1= new User({
-    name : NaMe
 
-});
-u1.save();
+
 
 
 
@@ -42,7 +37,11 @@ app.get("/", function(req,res){
 app.post("/", function(req,res){
     NaMe=req.body.uname;
     console.log(NaMe);
-    res.send(NaMe);
+    const cu = new user({
+        name: NaMe
+    });
+
+    cu.save();
 })
 
 app.listen(3000, function(){
